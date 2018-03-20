953e207ac6e4cefafb8e46c3532b0127a160e2f9
Zicrael <13ccdd@gmail.com>
2018-03-20 17:25:53 +0200

    init template

---
+++ src/sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-tree-component',
+  template: `<h1>Sample component!!</h1>`
+})
+export class SampleComponent {
+
+  constructor() {
+  }
+
+}
